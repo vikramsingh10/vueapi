24c2806441776ac73b2dd48fc030d0f6afc38a15
Vikram Singh <38415955+vikramsingh10@users.noreply.github.com>
2020-09-09 02:37:54 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <upper />
+  
+  </div>
+</template>
+
+<script>
+ import upper from './components/upper';
+
+ export default {
+   name: 'app',
+   components: {
+     upper
+   }
+
+ }
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: grey;
+  color: white;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/upper.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+       
+        <form @submit="ucase">
+        <input type="text" v-model="title" name="title" placeholder="enter text to be converted in UPPERCASE">
+          
+        <input type="submit" value="Submit" class="btn" />
+        
+        </form>
+        <h3> {{ustring}} </h3>  
+    </div>
+</template>
+
+<script>
+export default {
+    name: "upper",
+    data() {
+        return {
+            title: '',
+            ustring: ''
+        }
+    },
+    methods: {
+        ucase: function (e) {
+            e.preventDefault();
+            this.ustring = this.title.toUpperCase();
+            this.title = '';        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+
+    input[type="submit"] {
+        flex: 2;
+    }
+
+</style>
